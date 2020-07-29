0c6d28dd50ca645f0e654ddfede4ea9374ee3dd7
Chenyufeng <411610370@qq.com>
2020-07-29 15:17:32 +0800

    1

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -6,11 +6,18 @@ import { DocComponent } from './doc/doc.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { BlankComponent } from 'src/app/modules/blank/blank.component';
+import { DataComponent } from './data/data.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'index' },
   { path: 'index', component: IndexComponent, },
-  { path: 'doc', component: BlankComponent, }
+  { path: 'doc', component: DocComponent, },
+  { path: 'data', component: DataComponent, },
+  { path: 'alarm', component: BlankComponent, },
+  { path: 'repair', component: BlankComponent, },
+  { path: 'regulation', component: BlankComponent, },
+  { path: 'forum', component: BlankComponent, },
+  { path: 'rank', component: BlankComponent, },
 ];
 
 @NgModule({
@@ -18,6 +25,7 @@ const routes: Routes = [
     BlankComponent,
     IndexComponent,
     DocComponent,
+    DataComponent,
   ],
   imports: [
     IonicModule,
